77c50d59c8b3714f05cb06068e3084e51e7486ef
nower16 <nuzhatnower@iut-dhaka.edu>
2023-07-25 21:21:50 +0600

    fuc

--- bbstep.py
+++ bbstep.py
@@ -1,1 +1,27 @@
-print ("Hello world")
+#list function
+lucky_number = [4,8,15,16,23,42]
+friends = ['sami','afrida', 'jim']
+
+friends2 = friends.copy()
+
+print(friends2)
+
+#tuples
+
+coordinates = (4,5)
+
+print(coordinates[0])
+
+#funtion
+
+def greeting(name, age) :
+    print("Hello "+ name + ", your age is "+ age)
+    
+greeting('mike', '35')
+
+#return statement
+
+def cube(num):
+    return num*num*num
+    
+print(cube(3))
